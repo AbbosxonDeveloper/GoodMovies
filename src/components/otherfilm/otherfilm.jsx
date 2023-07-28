ab97e7e57cc116c7e12ea4c3b94cd45f3131b172
AbbosxonDeveloper <abbosxonoripov3@gmail.com>
2023-07-28 19:45:24 +0500

    change path

--- src/components/otherfilm/otherfilm.jsx
+++ src/components/otherfilm/otherfilm.jsx
@@ -6,19 +6,19 @@ export function OtherFilm() {
                 <h3 className='otherfilm--heading'>Другие фильмы</h3>
             <div className="otherfilm container">
                 <div className="otherfilm--box">
-                    <img src="./src/assets/harrypotter.png" alt="harrypotter movie" />
+                    <img src="../../assets/harrypotter.png" alt="harrypotter movie" />
                     <p className='otherfilm--text'>Гарри Поттер 20 лет спустя: Возвращение в Хогвартс</p>
                 </div>
                 <div className="otherfilm--box">
-                    <img src="./src/assets/fasterthanbullet.png" alt="fasterthanbullet movie" />
+                    <img src="../../assets/fasterthanbullet.png" alt="fasterthanbullet movie" />
                     <p className='otherfilm--text'>Быстрее пули</p>
                 </div>
                 <div className="otherfilm--box">
-                    <img src="./src/assets/morthman.png" alt="morthman movie" />
+                    <img src="../../assets/morthman.png" alt="morthman movie" />
                     <p className='otherfilm--text'>Варяг</p>
                 </div>
                 <div className="otherfilm--box">
-                    <img src="./src/assets/ancharted.png" alt="ancharted movie" />
+                    <img src="../../assets/ancharted.png" alt="ancharted movie" />
                     <p className='otherfilm--text'>Анчартед</p>
                 </div>
             </div>
